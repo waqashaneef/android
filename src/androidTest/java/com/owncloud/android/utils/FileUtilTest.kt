8ac58721ce9be3f10e8b4ec49fb4a2c90d71f2a5
Andy Scherzinger <info@andy-scherzinger.de>
2020-10-02 17:15:58 +0200

    add tests
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

---
+++ src/androidTest/java/com/owncloud/android/utils/FileUtilTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Nextcloud Android client application
+ *
+ * @author Andy Scherzinger
+ * Copyright (C) 2020 Andy Scherzinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.owncloud.android.utils
+
+import com.owncloud.android.AbstractIT
+import org.junit.Assert
+import org.junit.Test
+import java.io.File
+
+class FileUtilTest : AbstractIT() {
+    @Test
+    fun assertNullInput() {
+        Assert.assertEquals("text returned when searchText was not empty",
+            "",
+            FileUtil.getFilenameFromPathString(null))
+    }
+
+    @Test
+    fun assertEmptyInput() {
+        Assert.assertEquals("text returned when searchText was not empty",
+            "",
+            FileUtil.getFilenameFromPathString(""))
+    }
+
+    @Test
+    fun assertFileInput() {
+        val file = getDummyFile("empty.txt")
+        Assert.assertEquals("text returned when searchText was not empty",
+            "empty.txt",
+            FileUtil.getFilenameFromPathString(file.absolutePath))
+    }
+
+    @Test
+    fun assertDotInput() {
+        val file = getDummyFile(".")
+        Assert.assertEquals("text returned when searchText was not empty",
+            ".",
+            FileUtil.getFilenameFromPathString(file.absolutePath))
+    }
+
+    @Test
+    fun assertSlashInput() {
+        val tempPath = File(FileStorageUtils.getTemporalPath(account.name) + File.pathSeparator + "folder" )
+        if (!tempPath.exists()) {
+            Assert.assertTrue(tempPath.mkdirs())
+        }
+        Assert.assertEquals("text returned when searchText was not empty",
+            "",
+            FileUtil.getFilenameFromPathString(tempPath.absolutePath))
+    }
+
+    @Test
+    fun assertDotFileInput() {
+        val file = getDummyFile(".dotfile.ext")
+        Assert.assertEquals("text returned when searchText was not empty",
+            ".dotfile.ext",
+            FileUtil.getFilenameFromPathString(file.absolutePath))
+    }
+
+    @Test
+    fun assertFolderInput() {
+        val tempPath = File(FileStorageUtils.getTemporalPath(account.name))
+        if (!tempPath.exists()) {
+            Assert.assertTrue(tempPath.mkdirs())
+        }
+
+        Assert.assertEquals("text returned when searchText was not empty",
+            "",
+            FileUtil.getFilenameFromPathString(tempPath.absolutePath))
+    }
+
+    @Test
+    fun assertNoFileExtensionInput() {
+        Assert.assertEquals("text returned when searchText was not empty",
+            "file",
+            FileUtil.getFilenameFromPathString("/just/a/file"))
+    }
+}
